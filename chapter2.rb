ba2943c1c5af894e6b555adffa32481dc5252b64
pomochieng <pomochieng@gmail.com>
2016-06-21 19:25:12 +0300

    Chapter2-Count:1

--- chapter2.rb
+++ chapter2.rb
@@ -23,3 +23,36 @@ sound_horn
 accelerate
 use_headlights ("high-beam")
 
+
+def print_area(length, width)
+  puts length * width
+end
+
+def order_soda(flavor, size = "medium", quantity = 1)
+  if quantity == 1
+    plural = "soda"
+  else
+    plural = "sodas"
+  end
+  puts "#{quantity} #{size} #{flavor} #{plural}, coming right up! "
+end
+
+order_soda("Orange")
+order_soda("lemon-lime", "small", 2)
+order_soda("grape", "large")
+
+#no dumb Questions
+def say_hello (name)
+  puts "Hello, #{name} !"
+end
+
+def use_headlights(brightness = "low-beam")
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for deer!"
+end
+
+use_headlights
+use_headlights("high-beam")
+
+
+
