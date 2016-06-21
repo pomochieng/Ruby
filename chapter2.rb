120557fdce5f3992a8cf8a20ae9a4d4650e53b0d
pomochieng <pomochieng@gmail.com>
2016-06-21 17:16:03 +0300

    Chapter 2

---
+++ chapter2.rb
@@ -0,0 +1,25 @@
+def print_sum(arg1, arg2)
+  print arg1 + arg2
+end
+
+
+def accelerate
+  puts "Stepping on the gas"
+  puts "Speeding up"
+end
+
+def sound_horn
+  puts "Pressing the horn button"
+  puts "Beep beep"
+end
+
+def use_headlights(brightness)
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for deer!"
+end
+
+
+sound_horn
+accelerate
+use_headlights ("high-beam")
+
