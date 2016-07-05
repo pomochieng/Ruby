fb445dae85364c0aafe326a7611cbec9d73f34cd
pomochieng <pomochieng@gmail.com>
2016-07-05 11:09:03 +0300

    Chp13 hello test

---
+++ hello_web.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+get('/hello') do
+  'Hello, web!'
+end
+
+get('/salut') do
+  'Salut web!'
+end
+
+get('/namaste') do
+  'Namasta, web!'
+end
+
+
+
+
+
+
+
---
+++ test_something.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+class TestSomething < Minitest::Test
+  def test_true_assertion
+    assert(true)
+  end
+  def test_false_assertion
+    assert(false)
+  end
+
+end
+
